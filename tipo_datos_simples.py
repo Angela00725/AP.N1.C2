d6cac964a28b69111c4d8376d272555e4aca84c0
Angela00725 <angelaperozo679@gmail.com>
2026-04-01 10:49:42 -0300

    agregando nuevo archivo

---
+++ tipo_datos_simples.py
@@ -0,0 +1,64 @@
+#tipos de datos en python
+
+print(type (5))
+print(type (3.2))
+print(type ("Hola"))
+print(type (True))
+
+#variable nuemero_real almacenará un dato numero entero 
+
+numero_entero = 0
+numero_real= 0
+numero_decimal = 0.0
+cadena_texto = "buen dia"
+valor_booleano = False
+
+
+print ("mi primer mensaje en python")
+
+print()
+print (numero_entero)
+print (type(numero_entero))
+
+print()
+print (numero_real)
+print (type(numero_real))
+
+print()
+print (numero_decimal)
+print (type(numero_decimal))
+
+print()
+print (cadena_texto)
+print (type(cadena_texto))
+
+print ()
+print (valor_booleano)
+print(type(valor_booleano))
+
+print ()
+valor_booleano = 5 > 3
+print (valor_booleano)
+print(type(valor_booleano))
+
+#concatenacion de cadenas de texto
+saludo = "Buen dia "
+nombre = "Angela"
+print()
+print (saludo + nombre)
+
+texto_de_una_linea_comillas_dobles = "Texto de una linea"
+
+texto_de_una_linea_comillas_simples = 'Texto de una linea´'
+
+texto_multiples_comillas_dobles = '''
+Linea 1 texto multiples lineas
+Linea 1 texto multiples lineas
+Linea 1 texto multiples lineas'''
+
+texto_multiples_comillas_triples = """
+Linea 1 texto multiples lineas
+Linea 1 texto multiples lineas
+Linea 1 texto multiples lineas"""
+
+print ()
---
+++ variables.py
@@ -0,0 +1,15 @@
+#saludo = "Buen dia "
+#nombre = input("ingrese su nombre")
+#print()
+#print (saludo + nombre)
+
+#Ingrese 2 numeros mediante el teclado y muestre el resutado de la suma
+
+numero1 = int(input("ingrese primer numero entero: "))
+numero2 = int(input ("ingrese segundo numero entero: "))
+resultado = numero1 + numero2 
+print("El resultado de los dos numeros al sumar es: " + str(resultado))
+
+
+
+
